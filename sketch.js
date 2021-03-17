8d65df0cb1825d024cc4ab0bc558395e5522f98e
shoei123 <80819233+shoei123@users.noreply.github.com>
2021-03-17 17:44:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://shoei123.github.io/forg123/model.json';
 
 
 function preload() {
